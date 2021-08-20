d4b53fe817a70f3fc6545c23148690163c97ae88
dismo1 <83262011+dismo1@users.noreply.github.com>
2021-08-21 02:14:56 +0300

    Edit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function iconsfn() {
     let links = document.getElementById('links');
   links.style["transition"] = '1s';
   links.style["transition-property"] = 'margin-top';
-  links.style["margin-top"] = '-200px';
+  links.style["margin-top"] = '-210px';
 
   }
 }
